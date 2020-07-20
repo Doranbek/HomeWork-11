e62da0b7e9eca4313ff4034de85e3ad59d01610f
Doranbek <67508373+Doranbek@users.noreply.github.com>
2020-07-20 19:52:05 +0600

    Add files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork11Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача № 1
+            Student student1 = new Student("Карл", "Маркс", "SH20", "Химия", 3, 25);
+            student1.Info();
+            
+            student1.Transfer("", "Физика");
+            student1.Info();
+            
+            student1.Transfer("SE20", "Экономика");
+            student1.Info();
+            
+            //Задача № 2
+            Cars car1 = new Cars("Audi", "A6", 2.3);
+            Cars car2 = new Cars("Honda", "Accord", "Black", 2.4);
+            car1.DisplayInfo();
+            car2.DisplayInfo();
+            
+            car1.Painting("Red");
+            car1.DisplayInfo();
+            
+            car1.Setting("White", 2.8);
+            car1.DisplayInfo();
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork11Class
+{
+    class Student
+    {
+        private string Name { get; set; }
+        private string FirstName { get; set; }
+        private string Group { get; set; }
+        private string Facultet { get; set; }
+        private int Cours { get; set; }
+        private int Age{ get; set; }
+        
+        public Student(string name, string firstName, string group, string facultet, int cours, int age)
+        {
+            Name = name;
+            FirstName = firstName;
+            Group = group;
+            Facultet = facultet;
+            Cours = cours;
+            Age = age;
+        }
+
+        public void Info()
+        {
+            
+            Console.WriteLine($"\nИмя студента:{Name}\nФамилия Студента:{FirstName}\n" +
+                $"Группа:{Group}\nФакультет:{Facultet}\nКурс:{Cours}\nВозраст:{Age}");
+        }
+
+        public void Transfer(string group, string facultet)
+        {
+            if (group != "")
+            {
+                Group = group;
+            }
+            else Error("group");
+
+            if (facultet != "")
+            {
+                Facultet = facultet;
+            }
+            else Error("facultet");
+
+        }
+        private void Error(string input)
+        {            
+            Console.WriteLine($"\nЗначение {input} пустое");
+        }
+        
+    }
+}
